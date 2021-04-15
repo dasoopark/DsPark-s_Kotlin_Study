f73afeb9b8821b9d5caa20b75ad7853323e03e52
dasoopark <ektn0514@gmail.com>
2021-04-15 23:15:08 +0900

    when 사용법
    kotlin if대신 when을 사용하여 코드 리펙토링

---
+++ src/Chap2/RefactoringEvallf.kt
@@ -0,0 +1,22 @@
+package Chap2
+
+interface Expr
+class Num(val value: Int) : Expr
+class Sum(val left: Expr, val right: Expr) : Expr
+
+fun eval(e: Expr): Int =
+    when (e) {
+        is Num ->
+            e.value
+        is Sum ->
+            eval(e.right) + eval(e.left)
+        else ->
+            throw IllegalArgumentException("Unknown expression")
+    }
+
+fun main(args: Array<String>) {
+    println(eval(Sum(Num(1), Num(2))))
+}
+
+//try kotlin 2.3 ->6_2
+//kotlin in action P96
